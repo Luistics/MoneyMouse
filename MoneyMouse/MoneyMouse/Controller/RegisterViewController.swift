07944b47361d702b787d8cd0d4b17fd6bbf62d0c
Eisen <shh419@nyu.edu>
2018-11-27 22:03:40 -0500

    Initial Commit with pods and few files added

---
+++ MoneyMouse/MoneyMouse/Controller/RegisterViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RegisterViewController.swift
+//  MoneyMouse
+//
+//  Created by Eisen Huang on 11/27/18.
+//  Copyright © 2018 edu.nyu. All rights reserved.
+//
+
+import Foundation
--- MoneyMouse/MoneyMouse/Controller/WelcomeViewController.swift
+++ MoneyMouse/MoneyMouse/Controller/WelcomeViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //If there is a logged in user, by pass this screen and go straight to Main ViewController
+        
+        
     }
 
 
