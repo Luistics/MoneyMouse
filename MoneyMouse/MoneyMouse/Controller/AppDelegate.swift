9de85a936b86e7818504b3c644fb29484060a446
Luis Olivar <lao294@nyu.edu>
2018-11-28 12:07:16 -0500

    Firebase setup

--- MoneyMouse/MoneyMouse/Controller/AppDelegate.swift
+++ MoneyMouse/MoneyMouse/Controller/AppDelegate.swift
@@ -12,16 +12,14 @@ import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        FirebaseApp.configure()
-        
-        let myDatabase = Database.database().reference()
-        return true
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?)
+        -> Bool {
+            FirebaseApp.configure()
+            return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
