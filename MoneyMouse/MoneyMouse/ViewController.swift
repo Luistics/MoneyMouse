555e9fcc66504354541ed71c619b60fe697e6d1c
Luis Olivar <lao294@nyu.edu>
2018-11-27 21:09:04 -0500

    Project init

---
+++ MoneyMouse/MoneyMouse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoneyMouse
+//
+//  Created by Luis Olivar on 11/27/18.
+//  Copyright © 2018 edu.nyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
