7b7ade54e02107a0d0dea7ec302b3d134271f1e5
Luis Olivar <lao294@nyu.edu>
2018-11-30 17:54:20 -0500

    Added sign out function.

--- MoneyMouse/MoneyMouse/Controller/HomeScreenViewController.swift
+++ MoneyMouse/MoneyMouse/Controller/HomeScreenViewController.swift
@@ -7,16 +7,31 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Firebase
 
 class HomeScreenViewController: UIViewController {
 
+    @IBOutlet weak var logOutButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func logOutAction(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+        }
+        catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let initial = storyboard.instantiateInitialViewController()
+        UIApplication.shared.keyWindow?.rootViewController = initial
+    }
+    
     /*
     // MARK: - Navigation
 
