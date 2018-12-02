7f6fcf51a1d7254632c310fa8db6c9c1e735ed68
Luis Olivar <lao294@nyu.edu>
2018-12-02 16:42:50 -0500

    AddBudgetViewController

---
+++ MoneyMouse/MoneyMouse/Controller/AddBudgetViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddBudgetViewController.swift
+//  MoneyMouse
+//
+//  Created by Luis Olivar on 12/2/18.
+//  Copyright © 2018 edu.nyu. All rights reserved.
+//
+
+import UIKit
+
+class AddBudgetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
