68787a15f303602ae935466ff3181c02d2a512a1
Luis Olivar <lao294@nyu.edu>
2018-12-05 14:54:36 -0500

    Setup for merge for version 0.1

--- MoneyMouse/MoneyMouse/Controller/AddExpenseViewController.swift
+++ MoneyMouse/MoneyMouse/Controller/AddExpenseViewController.swift
@@ -56,18 +56,33 @@ class AddExpenseViewController: UIViewController {
         //search through the budgets database for an entry that corresponds to user input title
         budgetsRef.observeSingleEvent(of:.value, with: { snapshot in
             if snapshot.hasChild(budgetTitleLower!){
+                let alert = UIAlertController(title: "Are you sure?", message: "Are you sure you want to add this expense?", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("Yes!", comment: "Default action"), style: .default, handler: { _ in
+                    
+                    let budgetToUpdate = snapshot.childSnapshot(forPath:budgetTitleLower! + "/currentAmount").value
+                    let categoryOfExpense = snapshot.childSnapshot(forPath: budgetTitleLower! + "/category").value
+                    let expenseData = ExpenseEntry(assignedBudget: budgetTitle!, expenseAmount: amountSpentFloat!, category: categoryOfExpense as! String, addedByUser: self.userEmail!, dateEntered: now)
+                    
+                    let budgetToUpdateFloat = (budgetToUpdate as AnyObject).floatValue
+                    let temp = budgetToUpdateFloat! + amountSpentFloat!
+                    
+                    let updateRef = Database.database().reference(withPath:"budgets/" + String(self.userID) + "/" + budgetTitleLower! + "/currentAmount")
+                    updateRef.setValue(temp)
+                    print(temp)
+                    expenseRef.setValue(expenseData.toAnyObject())
+                    
+                    NSLog("Expense Added")
+                    self.tabBarController?.selectedIndex = 0;
+                }))
                 
-                let budgetToUpdate = snapshot.childSnapshot(forPath:budgetTitleLower! + "/currentAmount").value
-                let categoryOfExpense = snapshot.childSnapshot(forPath: budgetTitleLower! + "/category").value
-                let expenseData = ExpenseEntry(assignedBudget: budgetTitle!, expenseAmount: amountSpentFloat!, category: categoryOfExpense as! String, addedByUser: self.userEmail!, dateEntered: now)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("No.", comment: "Default action"), style: .default, handler: { _ in
+                    NSLog("User tapped ok")
+                        
+                }))
+                    
+                self.present(alert, animated: true, completion: nil)
+                    
                 
-                let budgetToUpdateFloat = (budgetToUpdate as AnyObject).floatValue
-                let temp = budgetToUpdateFloat! + amountSpentFloat!
-                
-                let updateRef = Database.database().reference(withPath:"budgets/" + String(self.userID) + "/" + budgetTitleLower! + "/currentAmount")
-                updateRef.setValue(temp)
-                print(temp)
-                expenseRef.setValue(expenseData.toAnyObject())
             }
             else{
                 let alert = UIAlertController(title: "Oops!", message: "You currently do not have a budget called " + budgetTitle!, preferredStyle: .alert)
